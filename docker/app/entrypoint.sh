7025febb50f9c098759ef6014fe70ae6974750ae
kasuta96 <kasuta96@gmail.com>
2023-03-21 16:16:40 +0900

    Add build FE to docker & fix artisan cache

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
 # Laravel
-composer update
-php artisan config:cache
+composer install
 php artisan key:generate --force
 php artisan storage:link
 php artisan migrate --force --no-interaction
 php artisan db:seed --force --no-interaction
 php artisan optimize:clear
+php artisan cache:clear
+php artisan config:cache
 
 # supervisord
 supervisord -c /etc/supervisord.conf
 supervisorctl update
+
+# Front-end
+npm install
+npm run build
+
+# Run app
 php-fpm -F
